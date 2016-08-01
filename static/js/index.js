bfd45a94855f0d79f91e6b1938276cc2a50a3880
johnlinp <johnlinp@gmail.com>
2016-08-01 13:17:25 +0800

    Change mail address label dynamically

---
+++ static/js/index.js
@@ -0,0 +1,40 @@
+(function() {
+    var iterateRadios = function(radios, func) {
+        for (var i = 0; i < radios.length; ++i) {
+            var result = func(radios[i]);
+            if (result) {
+                return result;
+            }
+        }
+    };
+
+    var listenMailMethod = function() {
+        var methodRadios = document.getElementsByName('invitationMethod');
+        var addrLabel = document.getElementById('address-label');
+        var addrInput = document.getElementById('address-input');
+
+        var updateLabel = function() {
+            var mailMethod = iterateRadios(methodRadios, function(radio) {
+                if (radio.checked) {
+                    return radio.value;
+                }
+            });
+
+            if (mailMethod == 'email') {
+                addrLabel.innerHTML = addrLabel.getAttribute('data-email');
+                addrInput.setAttribute('type', 'email');
+            } else if (mailMethod == 'postal') {
+                addrLabel.innerHTML = addrLabel.getAttribute('data-postal');
+                addrInput.setAttribute('type', 'text');
+            }
+        };
+
+        iterateRadios(methodRadios, function(radio) {
+            radio.addEventListener('click', updateLabel);
+        });
+
+        updateLabel();
+    };
+
+    listenMailMethod();
+})();
